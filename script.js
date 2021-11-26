e779c47aa83674d6712a2aa52552d272a7e734ed
Cogentx <code@cogentx.com>
2021-11-26 17:07:22 -0330

    mobile menu

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const btn = document.querySelector('#menu-btn');
+const mobileBtn = document.querySelector('#mobile-menu');
 
 function navToggle() {
   btn.classList.toggle('open');
+  mobileBtn.classList.toggle('hidden');
+  document.body.classList.toggle('no-scroll');
 }
 
 btn.addEventListener('click', navToggle);
-
-
-
-
