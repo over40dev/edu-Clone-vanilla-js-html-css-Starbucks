28379a826bf6b68c0228e9831456d4e9af57deef
Cogentx <code@cogentx.com>
2021-11-25 20:40:25 -0330

    Nav Toggle JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('loaded')
+const btn = document.querySelector('#menu-btn');
+
+function navToggle() {
+  btn.classList.toggle('open');
+}
+
+btn.addEventListener('click', navToggle);
+
+
+
+
